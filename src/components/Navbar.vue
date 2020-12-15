7f8067dc510491f583028553461205a8da8feed0
hooty868 <hooty868@gmail.com>
2020-12-15 13:19:29 +0800

    feat: add navbar vue
    navbar need to modify for twitter project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+    <router-link class="navbar-brand" to="/admin"> 餐廳評論網 </router-link>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div id="navbarSupportedContent" class="navbar-collapse collapse">
+      <div class="ml-auto d-flex align-items-center">
+        <!-- is user is admin -->
+        <router-link to="#" class="text-white mr-3" v-if="currentUser.isAdmin">
+          管理員後台
+        </router-link>
+
+        <!-- is user is login -->
+        <template v-if="isAuthenticated">
+          <router-link to="#" class="text-white mr-3">
+            {{ currentUser.name || "使用者" }} 您好
+          </router-link>
+          <button
+            type="button"
+            class="btn btn-sm btn-outline-success my-2 my-sm-0"
+            @click="logout"
+          >
+            登出
+          </button>
+        </template>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  // Vue 會在沒有資料時使用此預設值
+
+  computed: {
+    ...mapState(["currentUser", "isAuthenticated"]),
+  },
+  methods: {
+    logout() {
+      this.$store.commit("revokeAuthentication");
+      this.$router.push("/signin");
+    },
+  },
+};
+</script>
